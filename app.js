22060b6ba3050abf59ec6276fc22aa233a7323ec
Ventzzz <vicenteherreraaranguiz89@gmail.com>
2025-04-21 00:33:38 -0400

    modificando app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.post('/login', async (req, res) => {
 
   try {
     const result = await pool.query(
-      'SELECT * FROM users WHERE username = $1 AND password = $2',
+      'SELECT * FROM usuarios WHERE nombre = $1 AND contrasena = $2',
       [username, password]
     );
 
